4b3b3da3c6ffb9df3ef720d8e09496719a8c0b7d
Kim-chanmi <chanmi1320@gmail.com>
2022-11-30 18:16:12 +0900

    2022.11.30

--- src/components/layout/KakaoMap.jsx
+++ src/components/layout/KakaoMap.jsx
@@ -11,7 +11,7 @@ const KakaoMap = () => {
       };
 
     // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
-    let map = new kakao.maps.Map(mapContainer, mapOption);
+    let map = new kakao.maps.Map(mapContainer, mapOption); //eslint-disable-line no-unused-vars
   }, []);
 
   return <div id="map" style={{ width: "500px", height: "400px" }}></div>;
